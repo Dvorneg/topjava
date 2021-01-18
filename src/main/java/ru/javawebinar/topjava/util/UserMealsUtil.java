560f59f28e321eb8cd1e9425bc358747eb2d79b1
Dvornik <41857247+Dvorneg@users.noreply.github.com>
2021-01-18 22:05:24 +0300

    filteredByCycles

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -1,13 +1,14 @@
 package ru.javawebinar.topjava.util;
 
+
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -27,10 +28,25 @@ public class UserMealsUtil {
 //        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
+    /*     -  должны возвращаться только записи между `startTime` и `endTime`
+            -  поле `UserMealWithExcess.excess` должно показывать,
+    превышает ли сумма калорий за весь день значение `caloriesPerDay`*/
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with excess. Implement by cycles
-        return null;
+        List<UserMealWithExcess> resault = new ArrayList();
+        Map<LocalDate, Integer> dayCalories = new HashMap<>();
+        for (UserMeal meal : meals) {
+            dayCalories.put(meal.getDateTime().toLocalDate(), meal.getCalories());
+        }
+        for (UserMeal meal : meals) {
+            if ( TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(),startTime, endTime ))  {
+                resault.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), meal.getCalories() < caloriesPerDay));
+            }
+        }
+        return resault;
     }
+/*    UserMealWithExcess{dateTime=2020-01-30T10:00, description='Завтрак', calories=500, excess=true}
+    UserMealWithExcess{dateTime=2020-01-31T10:00, description='Завтрак', calories=1000, excess=true}*/
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO Implement by streams
