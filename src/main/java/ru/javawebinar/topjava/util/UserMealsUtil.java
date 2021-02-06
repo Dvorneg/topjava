646ec293a805d4363d3572671e1b24e1fab9c0ee
Dvornik <41857247+Dvorneg@users.noreply.github.com>
2021-02-06 14:50:43 +0300

    Refacoring all

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -1,13 +1,15 @@
 package ru.javawebinar.topjava.util;
 
+
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -24,16 +26,57 @@ public class UserMealsUtil {
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
 
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
+    /*task
+ENG:
+         - only records between `startTime` and `endTime`
+            -   the field  `UserMealWithExcess.excess` should show,
+    whether the sum of calories for the whole day is greater than the value of  `caloriesPerDay`
+*RU:
+    - должны возвращаться только записи между `startTime` и` endTime`
+            - поле `UserMealWithExcess.excess` должно показывать,
+    больше ли сумма калорий за весь день значение `caloriesPerDay`
+    */
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
+        Map<LocalDate, Integer> dayCalories = new HashMap<>();
+        for (UserMeal meal : meals) {
+            if (dayCalories.get(meal.getDateTime().toLocalDate()) == null)
+                dayCalories.put(meal.getDateTime().toLocalDate(), meal.getCalories());
+            else
+                dayCalories.put(meal.getDateTime().toLocalDate(), dayCalories.get(meal.getDateTime().toLocalDate()) + meal.getCalories());
+        }
+        List<UserMealWithExcess> result = new ArrayList<>();
+        for (UserMeal meal : meals) {
+            if (TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+                result.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
+                        dayCalories.get(meal.getDateTime().toLocalDate()) > caloriesPerDay));
+            }
+        }
+        return result;
     }
 
+    /* return = UserMealWithExcess{dateTime=2020-01-30T10:00, description='Завтрак', calories=500, excess=false}
+        UserMealWithExcess{dateTime=2020-01-31T10:00, description='Завтрак', calories=1000, excess=true}*/
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO Implement by streams
-        return null;
+
+        Map<LocalDate, Integer> dayCalories =
+                meals.stream()
+                        .collect(Collectors.groupingBy(
+                                um -> um.getDateTime().toLocalDate(),
+                                Collectors.summingInt(UserMeal::getCalories)));
+
+        return meals.stream()
+                .filter(um -> TimeUtil.isBetweenHalfOpen(um.getDateTime().toLocalTime(),
+                        startTime,
+                        endTime))
+                .map(um -> new UserMealWithExcess(um.getDateTime(),
+                        um.getDescription(),
+                        um.getCalories(),
+                        dayCalories.get(um.getDateTime().toLocalDate()) > caloriesPerDay
+                ))
+                .collect(Collectors.toList());
     }
+
 }
