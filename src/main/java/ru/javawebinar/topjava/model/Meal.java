6bd35295f6f4bd23ffc2896ae95c266f235a5fc1
Dvornik <41857247+Dvorneg@users.noreply.github.com>
2021-02-11 21:11:08 +0300

    WEll open update, with data

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -13,7 +13,7 @@ public class Meal {
 
     private final int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories ) {
+    public Meal(int id, LocalDateTime dateTime, String description, int calories ) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
