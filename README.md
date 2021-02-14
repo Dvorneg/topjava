9e696fcb45e05a536330ab3c5b945cc19ef324ef
Dvornik <41857247+Dvorneg@users.noreply.github.com>
2021-02-14 23:46:07 +0300

    Первый пункт пока что. (и без стрим)

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Урок 2 : https://github.com/JavaWebinar/topjava/blob/doc/doc/lesson02.md
+ AbstractUserController-> по аналогии Meal контролер
+ 
 Java Enterprise Online Project 
 ===============================
 Разработка полнофункционального Spring/JPA Enterprise приложения c авторизацией и правами доступа на основе ролей с использованием наиболее популярных инструментов и технологий Java: Maven, Spring MVC, Security, JPA(Hibernate), REST(Jackson), Bootstrap (css,js), datatables, jQuery + plugins, Java 8 Stream and Time API и хранением в базах данных Postgresql и HSQLDB.
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -5,41 +5,60 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.web.user.AbstractUserController;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+/*@Repository - по аналогии с меал, там нет этого, по заданию, но надо поставить.... */
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>(); //Добавляем для хранения
+    private final AtomicInteger counter = new AtomicInteger(0); //add  counter
 
     @Override
     public boolean delete(int id) {
+        repository.remove(id);
         log.info("delete {}", id);
         return true;
     }
 
     @Override
     public User save(User user) {
+        repository.put(counter.getAndIncrement(), user);
         log.info("save {}", user);
         return user;
     }
 
     @Override
     public User get(int id) {
+        User user = repository.get(id);
         log.info("get {}", id);
-        return null;
+        return user;
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return new ArrayList<User>( repository.values());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
+        //return UserService.getByEmail(email);
+        // TODO: передалать на stream 14.02.2021
+        //проййтись надо по колллекции , и вернуть только то, где е-майл, нужный  нам.
+        for (User user : repository.values()) {
+            if (user.getEmail().equals(email))
+                return user;
+        }
         return null;
     }
 }
